45d000919aa8b4c26d0ae16ef3ca8c97d4829150
paulstoma <49036526+paulstoma@users.noreply.github.com>
2021-03-31 15:17:20 -0400

    Delete Driver and Vendor.sql

--- Driver and Vendor.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO Vendor (V_Name, V_Address, V_PhoneNumber, V_ZipCode, V_Password,V_AccCreateDate, V_SSN, V_License) VALUES 
-("Michigan Laundromart","vendor@gmail.com","(555) 555-1234",48010,"root", current_timestamp,"111-22-3333",1232131);
-
-INSERT INTO Driver (D_Name, D_Address, D_PhoneNumber, D_ZipCode, D_Password,D_AccCreateDate, D_SSN, D_License) VALUES 
-("Michigan Best Driver","driver@gmail.com","(555) 555-1256",48015,"root", current_timestamp,"111-22-3333",1232131);
